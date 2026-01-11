487135868f2dd9902cbdf406903e406e268c786d
Zobayer Abdullah <zobayer.abdullah9999@gmail.com>
2026-01-11 16:45:17 +0600

    Update ResolveTenant.php

--- app/Http/Middleware/ResolveTenant.php
+++ app/Http/Middleware/ResolveTenant.php
@@ -6,6 +6,7 @@ use App\Models\Tenant;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+
 class ResolveTenant
 {
     public function handle(Request $request, Closure $next): Response
