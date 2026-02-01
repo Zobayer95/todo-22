cc9454b2bc60c12c29c4bcc23584096ddddb35d8
Zobayer Abdullah <zobayer.abdullah9999@gmail.com>
2026-02-01 15:18:39 +0600

    Update ResolveTenant.php

--- app/Http/Middleware/ResolveTenant.php
+++ app/Http/Middleware/ResolveTenant.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ResolveTenant 
-{
+{ 
     public function handle(Request $request, Closure $next): Response
     {
         $tenantId = $request->header('X-Tenant-ID');
