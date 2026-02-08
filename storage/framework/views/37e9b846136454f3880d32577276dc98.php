2624fbcb2af2c24431136c21b17494b5dc55920b
Zobayer Abdullah <zobayer.abdullah9999@gmail.com>
2026-02-08 16:42:50 +0600

    Fix indentation in HTML viewport meta tag

--- storage/framework/views/37e9b846136454f3880d32577276dc98.php
+++ storage/framework/views/37e9b846136454f3880d32577276dc98.php
@@ -2,7 +2,7 @@
 <html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
     <head>   
         <meta charset="utf-8">      
-        <meta name="viewport" content="width=device-width, initial-scale=1">  
+        <meta name="viewport" content="width=device-width, initial-scale=1">   
              
         <title><?php echo e(config('app.name', 'Laravel')); ?></title>   
             
